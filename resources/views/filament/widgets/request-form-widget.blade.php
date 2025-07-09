545d16df43a5afc7b18ee36773eb81f2abdfe98f
Gabi00720 <abcsi.o007@gmail.com>
2025-07-09 16:47:50 +0200

    formating

--- resources/views/filament/widgets/request-form-widget.blade.php
+++ resources/views/filament/widgets/request-form-widget.blade.php
@@ -3,25 +3,37 @@
         <form action="{{ url('/requests') }}" method="POST" enctype="multipart/form-data" class="space-y-4 p-4">
             @csrf
 
-            <label>Rendszám(ok)</label>
-            <input type="text" name="license_plates[]" required class="w-full border rounded p-2">
+            <div>
+                <label>Rendszám(ok)</label>
+                <input type="text" name="license_plates[]" required class="w-full border rounded p-2">
+            </div>
 
-            <label>Mettől</label>
-            <input type="datetime-local" name="from_date" required class="w-full border rounded p-2">
+            <div>
+                <label >Mettől</label>
+                <input type="datetime-local" name="from_date" required class="w-full border rounded p-2">
+            </div>
 
-            <label>Meddig</label>
-            <input type="datetime-local" name="to_date" required class="w-full border rounded p-2">
+            <div>
+                <label>Meddig</label>
+                <input type="datetime-local" name="to_date" required class="w-full border rounded p-2">
+            </div>
 
-            <label>Dolgozók nevei</label>
-            <input type="text" name="people[]" required class="w-full border rounded p-2">
+            <div>
+                <label>Dolgozók nevei</label>
+                <input type="text" name="people[]" required class="w-full border rounded p-2">
+            </div>
 
-            <label>Helyszín</label>
-            <input type="text" name="location" required class="w-full border rounded p-2">
+            <div>
+                <label>Helyszín</label>
+                <input type="text" name="location" required class="w-full border rounded p-2">
+            </div>
 
             <input type="hidden" name="status" value="Feldolgozás" />
 
-            <label>Megjegyzés</label>
-            <textarea name="comment" class="w-full border rounded p-2"></textarea>
+            <div>
+                <label>Megjegyzés</label>
+                <textarea name="comment" class="w-full border rounded p-2"></textarea>
+            </div>
 
             <button type="submit" class="px-4 py-2 bg-primary-600 text-white rounded">Beküldés</button>
         </form>
